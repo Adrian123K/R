dee3455c1ef693fa06dc60edde7857d53981cf19
FatherA <seojun.a.kim@gmail.com>
2020-06-15 11:30:45 +0900

    modify #1

--- R_class3(200615).R
+++ R_class3(200615).R
@@ -42,3 +42,16 @@ year(x$hiredate)
 attach(x)
 tapply(sal,list(loc,year(hiredate)),sum)
 
+#84
+rs=tapply(sal,list(loc,year(hiredate)),sum)
+rs[is.na(rs)]=0
+rs
+
+#85
+barplot(rs, col=rainbow(4), legend=rownames(rs),beside=T,args.legend=list(x='topright',bty='n',inset=c(-0.01,0)))
+
+#87
+install.packages("")
+line=read.csv('1-4호선승하차승객수.csv',header=T)
+t1 <- gvisMotionChart(line, idvar="line_no", timevar="time")
+plot(t1)
