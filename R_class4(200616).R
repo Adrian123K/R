053f9a4b9db36b57c1835e69eb2f146db27d90a5
FatherA <seojun.a.kim@gmail.com>
2020-06-16 16:11:13 +0900

    final modified

--- R_class4(200616).R
+++ R_class4(200616).R
@@ -1,6 +1,6 @@
 getwd()
 
-###########################################R shiny 실습#####################################
+###########################################R shiny 실습###############################################
 install.packages("ggplot2")
 install.packages("DT")
 install.packages("shiny")
@@ -298,7 +298,7 @@ server <-function(input, output) {
 # Run the app ----
 shinyApp(ui = ui, server = server)
 
-##############################################원형그래프 그리기######################################
+##############################################원형그래프 그리기##################################################
 
 ########################################121
 create_cnt=read.csv('창업건수.csv',head=T)
@@ -348,7 +348,7 @@ server <-function(input, output) {
 # Run the app ----
 shinyApp(ui = ui, server = server)
 
-############################## 선생님 답#########################
+############################## 선생님 답#################################
 library(shiny)
 library(datasets)
 
@@ -513,10 +513,89 @@ server <-function(input, output) {
 # Run the app ----
 shinyApp(ui = ui, server = server)
 
-################################################ 라인 그래프 그리기###############################
+################################################ 라인 그래프 그리기################################################
 
 ######################################## 128
 cars=c(1,3,6,4,9)
 trucks=c(2,5,4,5,12)
-plot(cars,type='o',col='blue',ylim=c(0,12))
-lines(trucks, type='o',col='red',ylim=c(0,12))
+plot(cars,type='o',col='blue',ylim=c(0,12),pch=21, lty=1)
+lines(trucks, type='o',col='red',ylim=c(0,12),pch=22,lty=2)
+
+######################################## 129
+cars=c(1,3,6,4,9)
+trucks=c(2,5,4,5,12)
+plot(cars,type='o',col='blue',ylim=c(0,12),pch=21, lty=1,axes=F,ann=F) 
+lines(trucks, type='o',col='red',ylim=c(0,12),pch=22,lty=2)
+# 새로운 축 추가
+axis(1, at=1:5, lab=c('월','화','수','목','금')) # axis(1:x축, at= : 범위, lab= : 축 라벨)
+axis(2) # axis(2) : y축
+box()
+g_range<-range(0,create_cnt$치킨집, drop_cnt$치킨집) # 0에서 부터 둘 중 큰 값까지
+legend(8,g_range[2],c('창업','폐업'), cex=0.7, col=c('blue','red'),pch=22, lty=1:2)
+
+######################################## 130
+create_cnt <- read.csv("창업건수.csv", header=T)
+drop_cnt <- read.csv("폐업건수.csv", header=T) 
+g <- create_cnt$치킨집
+m <- drop_cnt$치킨집
+g_range <- range(0, g, m)
+plot(g, type="o", col="blue", ylim=g_range, 
+     axes=FALSE, ann=FALSE)
+axis(1, at=1:10, lab=create_cnt$년도)
+axis(2)
+box()
+lines(m, type="o", pch=22, lty=2, col="red")
+title(main="치킨집 창업/폐업현황", col.main="red")
+title(xlab="year" )
+title(ylab="Total" )
+legend( 8, g_range[2], c("창업","폐업"), cex=0.8, col=c("blue","red"), pch=21:22, lty=1:2)
+
+######################################## 131
+library(shiny)
+library(datasets)
+create_cnt <- read.csv("창업건수.csv", header=T)
+drop_cnt <- read.csv("폐업건수.csv", header=T) 
+
+# Define UI ----
+ui <- fluidPage(    
+  # Give the page a title
+  titlePanel("연도별 업종별 창/폐업현황"),
+  # Generate a row with a sidebar
+  sidebarLayout(      
+    # Define the sidebar with one input
+    sidebarPanel(
+      selectInput("region", "업종:", 
+                  choices=colnames(create_cnt)[-1]),
+      hr(),
+      helpText("업종별 창업과 폐업 현황 보고서")
+    ),
+    # Create a spot for the barplot
+    mainPanel(
+      plotOutput("typePlot")  
+    )
+  )
+)
+
+# Define server logic ----
+server <-function(input, output) {
+  # Fill in the spot we created for a plot
+  output$typePlot <- renderPlot({
+    g <- create_cnt[,input$region]
+    m <- drop_cnt[,input$region]
+    g_range <- range(0, g, m)
+    plot(g, type="o", col="blue", ylim=g_range, 
+         axes=FALSE, ann=FALSE)
+    axis(1, at=1:10, lab=create_cnt$년도)
+    axis(2)
+    box()
+    lines(m, type="o", pch=22, lty=2, col="red")
+    title(main="업종별 창업/폐업현황", col.main="red")
+    title(xlab="year" )
+    title(ylab="Total" )
+    legend( 8, g_range[2], c("창업","폐업"), cex=0.8, col=c("blue","red"), pch=21:22, lty=1:2)
+  })
+}
+
+# Run the app ----
+shinyApp(ui = ui, server = server)
+
