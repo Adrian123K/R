ba330e863cf9693320ceb31030592f22e7ab947c
FatherA <seojun.a.kim@gmail.com>
2020-06-29 16:54:24 +0900

    pm 4class

--- R_class13 ML ch6-6 다중회귀분석실습 미국 의료비 데이터 (200629).R
+++ R_class13 ML ch6-6 다중회귀분석실습 미국 의료비 데이터 (200629).R
@@ -42,3 +42,51 @@ lm(expenses ~ .,  data= insurance)
 
 rs_m <- lm(expenses~., data=insurance)
 summary(rs_m)
+
+# 파생변수 age2 추가
+insurance$age2 <- insurance$age^2
+insurance
+cor( insurance[    , c("age","bmi","children","expenses",'age2')] ) 
+dev.new()
+pairs.panels(insurance[c('age','bmi','children','expenses','age2')])
+
+
+attach(insurance)
+rs_m2 <- lm(expenses ~ .,  data= insurance)
+summary(rs_m2)
+
+# 파생변수 bmi30 추가
+insurance$bmi30 <- ifelse(insurance$bmi>=30,1,0)
+head(insurance)
+
+cor( insurance[    , c("age","bmi","children","expenses",'age2','bmi30')] ) 
+dev.new()
+pairs.panels(insurance[c('age','bmi','children','expenses','age2','bmi30')])
+
+attach(insurance)
+rs_m2 <- lm(expenses ~ .,  data= insurance)
+summary(rs_m2)
+
+# 상호작용 파생변수 추가
+rs_m4 <- lm(expenses ~ age+age2+children+bmi+sex+bmi30*smoker+region,data=insurance)
+summary(rs_m4)
+
+#264
+dev.new()
+pairs.panels(insurance[c('age','bmi','children','','age2','bmi30','region','expenses')])
+summary(insurance$age)
+insurance$age3 <- ifelse(insurance$age>=39,1,0)
+insurance$child2 <- ifelse(insurance$children==2,1,0)
+insurance$region2 <- ifelse(insurance$region=='southeast',1,0)
+insurance$sex2 <- ifelse(insurance$sex=='male',1,0)
+cor( insurance[    , c('child2','age2','bmi30','region2','sex2','expenses')] ) 
+
+rs_m5 <- lm(expenses ~ age+age2+children+bmi+sex+smoker*age3*child2+smoker*bmi30*child2*age3*region2,data=insurance)
+summary(rs_m5)
+
+
+insurance$region2 <- ifelse(insurance$region == "northeast", 1, 0)
+insurance
+
+model_yj <- lm(expenses ~ age + age2 + children + bmi + sex + bmi30*smoker+region2*age, data=insurance)
+summary(model_yj)
